d4993084eba4369b74741db4c18e88ea5f8056c0
Miguel Castilleja <castilleja.miguel@gmail.com>
2022-07-27 13:10:57 -0500

    Actualizados comentarios y prevencion de click

--- src/js/logic.js
+++ src/js/logic.js
@@ -7,41 +7,41 @@ const myBalance = document.getElementById('balance');
 const insertName = document.getElementById('realName');
 
 const formWerehouse = document.getElementById('formWerehouse');
-const formWithdrawal = document.getElementById('formWithdrawal')
-const alertMessage = document.getElementById('alertText')
+const formWithdrawal = document.getElementById('formWithdrawal');
+const alertMessage = document.getElementById('alertText');
+
+const containerWh = document.getElementById('werehouse');
+const containerWd = document.getElementById('withdrawal');
 
 // Condicion para cargar pagina de usuario o regresar al login si no se encuentra datos en el localStorage
 if ( userName != null ){
+
     insertName.innerHTML = userName;
     myBalance.innerHTML = `$${userBalance}`;
     document.title = `ATM - ${userName}, tu saldo: $${userBalance}`
 
 } else {
+
     window.location.href="index.html"
+
 }
+
 
 // Obtiene la información de depositos
 formWerehouse.addEventListener('submit', (e) => {
     e.preventDefault();
     let amount = e.target.amountWerehouse.value;
     add( amount )
-    
-    setTimeout (function() {
-        location.reload(true);
-      }, 2300);
-
+    update();
 });
+
 
 // Obtiene la información de los retiros
 formWithdrawal.addEventListener('submit', (e) => {
     e.preventDefault();
     let amount = e.target.amountWithdrawal.value;
     subtract( amount );
-    
-    setTimeout (function() {
-        location.reload(true);
-      }, 2300);
-
+    update();
 });
 
 
@@ -51,33 +51,39 @@ const add = ( value ) => {
     // Se parsea el valor de userBalance a un numero
     let numberBalance = parseInt(userBalance);
     let addBalance;
-
+    
+    // Si el valor es un numero se suma al balance actual en caso contrario se pasa el NaN para validarse
     if ( !isNaN(parseInt(value)) ) {
         addBalance = parseInt(value) + numberBalance;
     } else {
         addBalance = parseInt(value);
     }
 
+    // Se valida que la suma del balance sea un numero, sea menor o igual a 990, que no se ingrese un numero negativo, que el valor no sea vacio y que sea diferente de 0
     if( !isNaN(addBalance) && addBalance <= 990  && Math.sign(value) != -1 && value != "" && value != 0 ){
-
+        
+        // Se actualiza el balance y se agrega la alerta 
         let newBalance = addBalance;
-
-        alertMessage.classList.add('alert-success')
-        localStorage.setItem('balance',  newBalance)
-        alertMessage.innerHTML = `Se ha echo un anexo a su cuenta de $${ value }, dajando un total de: $${ newBalance }`
+        alertMessage.classList.add('alert-success');
+        // Se actualiza el balance en el localStorage
+        localStorage.setItem('balance',  newBalance);
+        preventClick();
+        alertMessage.innerHTML = `Se ha echo un anexo a su cuenta de $${ value }, dajando un total de: $${ newBalance }`;
 
     } else if( isNaN(addBalance) || Math.sign(value) == -1 || value == "" || value == 0){
         
-        alertMessage.classList.add('alert-danger')
-        alertMessage.innerHTML = `Lo sentimos el numero ingresado no es un valor valido`
+        alertMessage.classList.add('alert-danger');
+        alertMessage.innerHTML = `Lo sentimos el numero ingresado no es un valor valido`;
     
     } else {
 
-        alertMessage.classList.add('alert-danger')
-        alertMessage.innerHTML = `Lo sentimos la cantidad de $${ value }, no puede ser agregada a su cuenta porque excede el limite de su cuenta ($990)`
+        alertMessage.classList.add('alert-danger');
+        alertMessage.innerHTML = `Lo sentimos la cantidad de $${ value }, no puede ser agregada a su cuenta porque excede el limite de su cuenta ($990)`;
     
     }
 }
+
+
 
 // Validar Logica de restar
 const subtract = ( value ) => {
@@ -86,29 +92,34 @@ const subtract = ( value ) => {
     let numberBalance = parseInt(userBalance);
     let substracBalance;
 
+    // Si el valor es un numero se suma al balance actual en caso contrario se pasa el NaN para validarse
     if ( !isNaN(parseInt(value)) ) {
         substracBalance = numberBalance - parseInt(value);
     } else {
         substracBalance = parseInt(value);
     }
 
+    // Se valida que la suma del balance sea un numero, sea mayor igual a 10, no sea un numero negativo, que el valor agregado no este vacio y que sea diferente de 0
     if ( !isNaN(substracBalance) && substracBalance >= 10 && Math.sign(value) != -1 && value != "" && value != 0 ) {
-
+        
+        // Se actualiza el balance y se agrega la alerta 
         let newBalance = substracBalance;
 
-        alertMessage.classList.add('alert-success')
-        localStorage.setItem('balance',  newBalance)
-        alertMessage.innerHTML = `Se ha un retiro satisfactorio de $${ value }, dajando un total de: $${ newBalance }`
+        alertMessage.classList.add('alert-success');
+        // Se actualiza el balance en el localStorage
+        localStorage.setItem('balance',  newBalance);
+        preventClick();
+        alertMessage.innerHTML = `Se ha un retiro satisfactorio de $${ value }, dajando un total de: $${ newBalance }`;
 
     } else if( isNaN(substracBalance) || Math.sign(value) == -1 || value == "" || value == 0 ){
         
-        alertMessage.classList.add('alert-danger')
-        alertMessage.innerHTML = `Lo sentimos el numero ingresado no es un valor valido`
+        alertMessage.classList.add('alert-danger');
+        alertMessage.innerHTML = `Lo sentimos el numero ingresado no es un valor valido`;
     
     }  else {
 
-        alertMessage.classList.add('alert-danger')
-        alertMessage.innerHTML = `Lo sentimos la cantidad de $${ value }, no puede ser retirada de su cuenta pues el monto minimo son ($10)`
+        alertMessage.classList.add('alert-danger');
+        alertMessage.innerHTML = `Lo sentimos la cantidad de $${ value }, no puede ser retirada de su cuenta pues el monto minimo son ($10)`;
     
     }
 }
@@ -121,3 +132,16 @@ exitAtm.addEventListener('click', (e) => {
     window.location.href="index.html"
 })
 
+
+
+let update = () => {
+    setTimeout (function() {
+        location.reload(true);
+      }, 2300);
+}
+
+
+let preventClick = () => {
+    containerWd.removeAttribute('data-bs-toggle');
+    containerWh.removeAttribute('data-bs-toggle');
+}
